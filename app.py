fe99a8663d5e67650c649722bdd6dc7a5d8fe9c0
altescy <yas.gtr@gmail.com>
2017-01-28 17:23:28 +0900

    add redirect

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Tue Jan 24 22:26:35 2017
 """
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 
 from model import Seq2SeqAttention
@@ -15,19 +15,26 @@ from utilities.word2id import Word2ID
 
 
 app = Flask(__name__)
-
+app.secret_key = 'secret_key'
 
 @app.route('/')
 def index():
+    if 'title' in session:
+        del session['title']
     return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
+@app.route('/post', methods=['POST'])
 def generate_abstract():
-    if request.method == 'POST':
-        title = request.form['title']
-        abst = generate([title], 'model/s2smodel.npz')[0]
-        return render_template('index.html', title=title, abst=abst)
-    return render_template('index.html')
+    session['title'] = request.form['title']
+    return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    if 'title' not in session:
+        return redirect('')
+    title = session['title']
+    abst = generate([title], 'model/s2smodel.npz')[0]
+    return render_template('index.html', title=title, abst=abst)
 
 
 
